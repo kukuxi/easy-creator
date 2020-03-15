0d77ef5642ef42a9fdcc108055d5fed77dd52ba7
kukuxi <522750923@qq.com>
2020-03-15 19:09:03 +0800

    [add] eslint

--- webpack.prod.js
+++ webpack.prod.js
@@ -19,7 +19,7 @@ module.exports = {
         rules: [
             {
                 test: /.js$/,
-                use: 'babel-loader'
+                use: ['babel-loader', 'eslint-loader']
             },
             {
                 test: /.css$/,
